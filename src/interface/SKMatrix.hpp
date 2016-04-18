71dd5e87274d8b813f3279dd72cc817f50501f60
jakenayeck <jan2150@columbia.edu>
2016-04-17 22:06:00 -0400

    fixed skmatrix

--- src/interface/SKMatrix.hpp
+++ src/interface/SKMatrix.hpp
@@ -3,24 +3,28 @@
 template <typename T>
 class SKMatrix {
     public:
-        //virtual C& data() const = 0; if we want this function then fix the other SKMatrix<T> instances
-        virtual T& mult(T& rhs) const = 0;
+        SKMatrix(){}
+        ~SKMatrix(){}
 
+        //virtual C& data() const = 0; if we want this function then fix the other SKMatrix<T> instances
+        virtual T& mult(T& rhs) const = 0; 
+
+        /*
         // Gaussian projection
         virtual T& rand_n(int row, int col, int mean, int std) const = 0;
         virtual T& elem_div(const T a) const = 0;
 
-        /* Count Sketch */
+        // Count Sketch 
         virtual std::vector<int>& flip_signs(const int col...) const = 0;
         virtual std::vector<int>& bucket(const int num_buckets) const = 0;
 
         virtual T& count_sketch(void) const = 0;
 
-        /* Regression */
+        // Regression 
         virtual T& concat(const T& col) const  = 0;
         virtual T& solve_x(const T& A, const T& B) const = 0;
 
-        /* TODO: K-SVD */
+        // TODO: K-SVD 
         virtual T& overridce_col(const int col, const SKMatrix& B) const = 0;
         virtual std::vector<T> qr_decompose() const = 0;
 
@@ -28,6 +32,7 @@ class SKMatrix {
         virtual std::vector<int>& dimensions(void) const = 0;
 
         virtual SKMatrix<T>& mult(SKMatrix<T>& rhs) const = 0;
+        */
 
 };
 
