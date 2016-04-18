e98ce86db72a499c92dbddc2ebe0c427f0eaa422
jakenayeck <jan2150@columbia.edu>
2016-04-18 04:57:05 -0400

    using std::move

--- src/armadillo/Arm_interface.hpp
+++ src/armadillo/Arm_interface.hpp
@@ -9,41 +9,46 @@ class Armadillo_Matrix: SKMatrix<Armadillo_Matrix, arma::mat>{
     public:
 
         Armadillo_Matrix(){
+            std::cout << "constructor" << std::endl; 
             matrix_data = mat(); 
         }
-        
+
         ~Armadillo_Matrix(){
         }
 
         Armadillo_Matrix(int r, int c){
             matrix_data = mat(r,c); 
         }
-        
+
         mat data() const{
             return mat(matrix_data); 
         }
-        
-        
+
+
         Armadillo_Matrix(mat other){
             this->matrix_data = other; 
         } 
 
         Armadillo_Matrix(const Armadillo_Matrix& other){
+            std::cout << "using copy operator" << std::endl; 
             auto temp = other.matrix_data; 
             this->matrix_data = mat(temp); 
         }
-        
+
         Armadillo_Matrix& operator=(const Armadillo_Matrix& other){
             std::cout << "using copy operator" << std::endl; 
             this->matrix_data = mat(other.matrix_data); 
             return *this; 
         }
-        
+
         Armadillo_Matrix(Armadillo_Matrix&& other){
+            std::cout << "using move operator" << std::endl; 
             this->matrix_data = other.matrix_data; 
+
+            //might not need to do this
             other.matrix_data = mat(); 
         }
-        
+
         Armadillo_Matrix& operator=(Armadillo_Matrix&& other){
             std::cout << "using move operator" << std::endl; 
             this->matrix_data = other.matrix_data;
@@ -51,13 +56,17 @@ class Armadillo_Matrix: SKMatrix<Armadillo_Matrix, arma::mat>{
             return *this; 
         }
 
-        Armadillo_Matrix rand_n(int row, int col, int mean, int std){
-            mat a(row, col); 
-            a.randn(); 
-            a = a*std + mean; 
-            auto b = Armadillo_Matrix(a); 
-            return b; 
-        }
+        Armadillo_Matrix rand_n(int row, int col, int mean, int std); 
+
 };
+
+Armadillo_Matrix Armadillo_Matrix::rand_n(int row, int col, int mean, int std){
+    std::cout << "filling with gaussian noise" << std::endl; 
+    mat a(row, col); 
+    a.randn(); 
+    a = a*std + mean; 
+    auto b = Armadillo_Matrix(a); 
+    return std::move(b); 
+}
 
 
--- src/armadillo/test/test_construction/sample1_unittest.cc
+++ src/armadillo/test/test_construction/sample1_unittest.cc
@@ -22,3 +22,13 @@ TEST(construct_arm_interface_row_col, TRIVIAL){
     //ignore this it's just to register test
     EXPECT_EQ(1,1); 
 }
+
+Armadillo_Matrix f(){
+    auto A = Armadillo_Matrix(); 
+    cout << A.data() << endl; 
+    return A.rand_n(3,4, 0, 1.0); 
+}
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS(); 
+}
