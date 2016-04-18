fdc263dbdf1a46ab80fa223b6143c3f5af9dd03e
jakenayeck <jan2150@columbia.edu>
2016-04-17 23:35:14 -0400

    made it hpp

---
+++ src/armadillo/Arm_interface.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "SKMatrix.hpp"
+#include <armadillo>
+
+using namespace arma; 
+class Armadillo_Matrix: SKMatrix<Armadillo_Matrix, arma::mat>{
+    mat matrix_data; 
+    public:
+    int row; 
+    int col; 
+
+    Armadillo_Matrix(){}
+    Armadillo_Matrix(int r, int c){
+        matrix_data = mat(r,c); 
+    }
+
+    mat data() const{
+        return mat(matrix_data); 
+    }
+
+    int get_row(){
+        return row; 
+    }
+
+};
+
