767964819f8dc7badb9442bbec1550cb75ca7df2
jakenayeck <jan2150@columbia.edu>
2016-04-18 04:32:53 -0400

    added reason for why the fuck valgrind is broken on my computer

--- src/armadillo/run_all_test.py
+++ src/armadillo/run_all_test.py
@@ -9,6 +9,10 @@ for file_name in list_files:
     (sd, se) = subprocess.Popen([command], stdout=subprocess.PIPE, cwd=file_name).communicate()
     print "stdout, stderr:"
     print sd, se
+
+
+    #hacky path to valgrind, regular valgrind is broken on my computer and I didn't want to change that path
+    #so I pointed to a dev version
     command2 = '/usr/local/Cellar/valgrind/HEAD/bin/valgrind --leak-check=yes ./' + file_name + '/sample1_unittest'
     print command2
     (sd, se) = subprocess.Popen([command2], stdout=subprocess.PIPE, shell=True).communicate()
