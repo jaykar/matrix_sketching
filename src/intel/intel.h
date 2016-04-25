60ccbe7564d8403773d22789c778b36de36960db
Adam Incera <aji2112@columbia.edu>
2016-04-25 14:10:18 -0400

    addition, rand_n working

--- src/intel/intel.h
+++ src/intel/intel.h
@@ -1,40 +1,115 @@
 #include "Matrix.hpp"
 #include "mkl.h"
+#include "mkl_blas.h"
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <chrono>
+#include <random>
 
 class IntelMatrix : SKMatrix<IntelMatrix>  {
     private:
-        double *data;
         int rows;
         int cols;
 
     public:
+        double *data;
         IntelMatrix() {
-            std::cout << "intel constructor" << std::endl;
+            //std::cout << "intel constructor" << std::endl;
             data = NULL;
             rows = 0;
             cols = 0;
         }
 
         IntelMatrix(const int row, const int col) {
-            std::cout << "intel parametric constructor" << std::endl;
+            //std::cout << "intel parametric constructor" << std::endl;
             data = (double *) mkl_malloc(row * col * sizeof(double), sizeof(double));
+            memset(data, 0, row * col * sizeof(double));
             rows = row;
             cols = col;
         }
 
         ~IntelMatrix() {
-            std::cout << "intel destructor" << std::endl;
+            //std::cout << "intel destructor: " << (long) data << std::endl;
             mkl_free(data);
         }
 
-        /*
-           IntelMatrix(const IntelMatrix& im);
-           IntelMatrix& operator=(const IntelMatrix& rhs); 
-        //IntelMatrix(IntelMatrix&& other);
+        IntelMatrix(const IntelMatrix& im) {
+            //std::cout << "copy constructor" << std::endl;
+            data = (double *) mkl_malloc(im.size() * sizeof(double), sizeof(double));
+            cblas_dcopy(im.size(), im.data, 1, data, 1);
+            rows = im.rows;
+            cols = im.cols;
+        }
 
-        //gpu parallaizable
-        IntelMatrix operator+(const IntelMatrix& rhs) const;
+        void init() {
+            int i, j;
+            if(!this->data) {
+                this->data = (double *) mkl_malloc(sizeof(double), sizeof(double));
+                this->rows = 1;
+                this->cols = 1;
+            }
+            memset(this->data, 0, this->size() * sizeof(double));
+        }
+
+        IntelMatrix& operator=(const IntelMatrix& rhs) {
+            if(this->data)
+                mkl_free(this->data);
+            this->rows = rhs.rows;
+            this->cols = rhs.cols;
+            this->data = (double *) mkl_malloc(rhs.size() * sizeof(double), sizeof(double));
+            cblas_dcopy(rhs.size(), rhs.data, 1, this->data, 1);
+            return *this;
+        }
+
+        int size() const {
+            return this->rows * this->cols;
+        }
+
+        IntelMatrix rand_n(int row = -1, int col = -1) {
+
+            if(row != -1)
+                this->rows = row;
+            if(col != -1)
+                this->cols = col;
+            if(this->data)
+                mkl_free(this->data);
+            this->data = (double *) mkl_malloc(this->size() * sizeof(double), sizeof(double));
+            int i;
+            unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+            std::mt19937 gen(seed);
+            std::normal_distribution<double> n;
+            
+            for(i = 0; i < this->size(); i++) {
+                this->data[i] = n(gen);
+                //std::cout << i << ": " << this->data[i] << std::endl;
+            }
+            
+            return *this;
+        }
+
+        std::vector<int> dimensions() const {
+            std::vector<int> ret = {this->rows, this->cols};
+            return ret;
+        }
+
+        /*
+        //IntelMatrix(IntelMatrix&& other);
+        */
+
+        //gpu parallelizable
+        IntelMatrix operator+(const IntelMatrix& rhs) const {
+            if(this->dimensions() != rhs.dimensions())
+                throw std::invalid_argument("mismatched dimensions");
+            IntelMatrix temp(*this);
+            std::cout << "temp: " << temp << std::endl;
+            cblas_daxpy(temp.size(), 1, rhs.data, 1, temp.data, 1);
+            return temp;
+        }
+
+
+        /*
+
         IntelMatrix operator-(const IntelMatrix& rhs) const;
 
         IntelMatrix& operator+=(const IntelMatrix& rhs); 
@@ -45,9 +120,6 @@ class IntelMatrix : SKMatrix<IntelMatrix>  {
 
         IntelMatrix& operator/(const IntelMatrix& rhs); 
         IntelMatrix& operator/=(const IntelMatrix& rhs); 
-
-        int size() const;
-        std::vector<int>& dimensions() const;
 
         IntelMatrix& mult(IntelMatrix& m) const;
 
@@ -68,5 +140,22 @@ class IntelMatrix : SKMatrix<IntelMatrix>  {
         IntelMatrix& override_col(const int col, const IntelMatrix& B) const;
         std::vector<IntelMatrix> qr_decompose() const;
         IntelMatrix& svd() const;
-        */
+         */
+
+        friend std::ostream& operator<<(std::ostream&os, const IntelMatrix& im);
 };
+
+std::ostream& operator<<(std::ostream&os, const IntelMatrix& im) {
+    int i;
+    std::ostringstream out;
+    for(i = 0; i < im.size(); i++) {
+        /*
+        if(i > 0 && i % im.cols == 0)
+            out << '\n';
+            */
+        out << std::to_string(im.data[i]);
+        out << '\t';
+    }
+    os << out.str();
+    return os;
+}
--- src/intel/main.cpp
+++ src/intel/main.cpp
@@ -1,7 +1,30 @@
 #include "intel.h"
 
 int main() {
-    IntelMatrix m;
-    IntelMatrix i(4, 5);
+    IntelMatrix a;
+    a.init();
+    std::cout << "a: ";
+    std::cout << a << std::endl;
+    IntelMatrix b(4, 3);
+    std::cout << "b: ";
+    std::cout << b << std::endl;
+    IntelMatrix c(b);
+    std::cout << "c: ";
+    std::cout << c << std::endl;
+    IntelMatrix d = c;
+    std::cout << "d: ";
+    std::cout << d << std::endl;
+    a.rand_n(3, 4);
+    b.rand_n(3, 4);
+    std::cout << "a: ";
+    std::cout << a << std::endl;
+    std::cout << "b: ";
+    std::cout << b << std::endl;
+    c = a + b;
+    std::cout << "c: ";
+
+    std::cout << c << std::endl;
+
+    //std::cout << c.data[2] << " " << a.data[2] + b.data[2] << std::endl;
     return 0;
 }
