b4c7689b8b5dd723d0b5f22064b1cc7be9ca5dfd
Howon <howonbyun@gmail.com>
2016-04-26 22:00:28 -0400

    operations commented

--- src/operations.hpp
+++ src/operations.hpp
@@ -5,11 +5,11 @@
 #include <vector>
 
 /**
-   * Funciton that returns a gaussian projection of given
-   * @param input SKMatrix to take gaussian projection of
-   * @param n a constant number of dimension to reduce input to
-   * @return gaussian projection of a given graph in n dimensions
-   */
+ * Funciton that returns a gaussian projection of given
+ * @param input SKMatrix to take gaussian projection of
+ * @param n a constant number of dimension to reduce input to
+ * @return gaussian projection of a given graph in n dimensions
+ */
 template <typename SKMatrix>
 SKMatrix gaussian_projection(const SKMatrix& input, const int n){
     int n_col = input.num_cols();
@@ -20,13 +20,13 @@ SKMatrix gaussian_projection(const SKMatrix& input, const int n){
 }
 
 /**
-   * Takes a linear regression on Ax=B and returns the solution
-   * using gaussian projection to sketch
-   * @param A matrix A in Ax=B
-   * @param B matrix B in Ax=B
-   * @param n a constant number of dimension to reduce A to
-   * @return approximated matrix from linear regression
-   */
+ * Takes a linear regression on Ax=B and returns the solution
+ * using gaussian projection to sketch
+ * @param A matrix A in Ax=B
+ * @param B matrix B in Ax=B
+ * @param n a constant number of dimension to reduce A to
+ * @return approximated matrix from linear regression
+ */
 template <typename SKMatrix>
 SKMatrix& lin_regress(const SKMatrix& A, const SKMatrix& B, const int n){
     int n_col = A.num_cols();
@@ -45,11 +45,11 @@ SKMatrix& lin_regress(const SKMatrix& A, const SKMatrix& B, const int n){
 }
 
 /**
-   * Count sketch algorithm in map-reduce
-   * @param A matrix to perform count sketch on
-   * @param n number of buckets/hash sets
-   * @return count sketch matrix
-   */
+ * Count sketch algorithm in map-reduce
+ * @param A matrix to perform count sketch on
+ * @param n number of buckets/hash sets
+ * @return count sketch matrix
+ */
 template <typename SKMatrix>
 SKMatrix count_sketch(const SKMatrix& A, const int n) {
     std::vector<std::vector<int> > buckets = A.bucket(n);
