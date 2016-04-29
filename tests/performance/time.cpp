d1286e7049f6d7938e9e19a7ff679780841dcad1
Howon <howonbyun@gmail.com>
2016-04-28 21:35:55 -0400

    compile should be fixed

--- tests/performance/time.cpp
+++ tests/performance/time.cpp
@@ -4,15 +4,15 @@
 #include <string>
 
 #include "operations.hpp"
-#include "sk_boost.hpp"
-#include "sk_arm.hpp"
-#include "sk_intel.hpp"
+#include "boost.hpp"
+#include "armadillo.hpp"
+#include "intel.hpp"
 
 using namespace std;
 
 template <typename T>
-void test(ofstream& myfile) {
-    string header = "Size, Instantiation, Rand_Fill, Mult, Elem_Div, Concat, Subtract, Accumulate, Gaussian Decomposition, Count Sketch\n";
+void test_perf(ofstream& myfile){
+    string header = "Size, Instantiation, Rand_Fill, Mult, Elem_Div, Concat, Subtract, Accumulate\n";
     cout << header;
     myfile << header;
 
@@ -52,8 +52,8 @@ void test(ofstream& myfile) {
 
         start = chrono::system_clock::now();
         matrix.accumulate();
-        std::cout << (chrono::system_clock::now() - start).count() << " ";
-        myfile << (chrono::system_clock::now() - start).count() << ',';
+        std::cout << (chrono::system_clock::now() - start).count() << "\n";
+        myfile << (chrono::system_clock::now() - start).count() << '\n';
 
         // T Q(1, 1);
         // T R(1, 1);
@@ -73,27 +73,60 @@ void test(ofstream& myfile) {
         // std::cout << (chrono::system_clock::now() - start).count() << '\n';
         // myfile << (chrono::system_clock::now() - start).count() << '\n';
     }
+
+    // myfile << '\n';
+    // myfile << "N x N Linear Regression \n";
+    // myfile << "Size, Intel, Armadillo, Boost \n";
+    // for(int matrix_size = 100; matrix_size <= 1000; matrix_size+=100){
+    //     T matrix(matrix_size, matrix_size);
+    //     T Q(1, 1);
+    //     T R(1, 1);
+    //     sketchy::ops::lin_regress<T>()
+    //     start = chrono::system_clock::now();
+    //     matrix1.qr_decompose(Q, R);
+    //     std::cout << matrix_size << " " << (chrono::system_clock::now() - start).count() << std::endl;
+    //     myfile << matrix_size << "," << (chrono::system_clock::now() - start).count() << '\n';
+    //     // start = chrono::system_clock::now();
+    //     // sketchy::arm arm_matrix(matrix_size, matrix_size);
+    //     // start = chrono::system_clock::now();
+
+    // }
+
+    // myfile << '\n';
+    // myfile << "N x N Count Sketch \n";
+    // myfile << "Size, Intel, Armadillo, Boost \n";
+    // for(int matrix_size = 100; matrix_size <= 1000; matrix_size+=100){
+    //     T matrix1(matrix_size, matrix_size);
+    //     T Q(1, 1);
+    //     T R(1, 1);
+    //     start = chrono::system_clock::now();
+    //     matrix1.qr_decompose(Q, R);
+    //     std::cout << matrix_size << " " << (chrono::system_clock::now() - start).count() << std::endl;
+    //     myfile << matrix_size << "," << (chrono::system_clock::now() - start).count() << '\n';
+    //     // start = chrono::system_clock::now();
+    //     // sketchy::arm arm_matrix(matrix_size, matrix_size);
+    //     // start = chrono::system_clock::now();
+
+    // }
 }
 
 int main(int argc, char *argv[]){
     ofstream myfile;
-
-    if(argc != 2 ) {
+    if(argc != 2 ){
         myfile.open ("construction_test1.csv");
     } else {
         myfile.open (argv[1]);
     }
-
     myfile << "Boost" << '\n';
-    test<sketchy::boost>(myfile) ;
+    test_perf<sketchy::boost>(myfile);
     myfile << '\n';
 
     myfile << "Armadillo" << '\n';
-    test<sketchy::arm>(myfile) ;
+    test_perf<sketchy::arm>(myfile);
     myfile << '\n';
 
     myfile << "Intel" << '\n';
-    test<sketchy::intel>(myfile) ;
+    test_perf<sketchy::intel>(myfile);
     myfile << '\n';
 
     myfile.close();
