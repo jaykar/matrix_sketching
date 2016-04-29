9ef75d3c0219254e504bd1483cb09472723ea331
Howon <howonbyun@gmail.com>
2016-04-29 10:14:21 -0400

    presentation ready

--- tests/operations/arm_testing/operations_test_arm.cpp
+++ tests/operations/arm_testing/operations_test_arm.cpp
@@ -7,49 +7,38 @@ using namespace std;
 
 //namespace bnu = boost::numeric::ublas;
 
-template<typename T>
-T rand_n(int total_size){
-    auto input = T();
-    input.rand_n(total_size, total_size);
-    return input;
+void ksvd_test(){
+    string x_label = "data_image";
+    cout << "loading x train" << endl;
+    sketchy::armadillo x_train = sketchy::armadillo(x_label);
+
+    sketchy::armadillo U;
+    sketchy::armadillo S;
+    sketchy::armadillo V;
+    sketchy::ops::k_svd(x_train, U, S, V, 5, 20);
+
+    U.save("u");
+    S.save("s");
+    V.save("v");
 }
-
-template<typename T>
-T transpose_mult(T input){
-    auto b = input;
-    b.transpose();
-    return input.mult(b);
-}
-
-int main(){
-
-    string x_label = "data_image"; 
-    cout << "loading x train" << endl; 
-    sketchy::armadillo x_train = sketchy::armadillo(x_label); 
-    auto vec_ans = sketchy::ops::k_svd(x_train, 5, 20); 
-
-    vec_ans[0].save("u"); 
-    vec_ans[1].save("s"); 
-    vec_ans[2].save("v"); 
-
     /*
-    cout << "loading y train" << endl; 
-    string y_label = "../../../../mnist/regularized_label_small"; 
-    sketchy::armadillo y_train = sketchy::armadillo(y_label); 
-    
-    cout << "solving for b" << endl;  
-    sketchy::armadillo b = x_train.solve_x(y_train);  
-    
-    cout << "solving for xtilde" << endl;  
-    sketchy::armadillo x_tilde = sketchy::ops::lin_regress<sketchy::armadillo>(x_train, y_train, 600); 
-            
-    //sketchy::armadillo y_pred = x_train.mult(x_tilde); 
-    //sketchy::armadillo y_pred2 = x_train.mult(b); 
+    cout << "loading y train" << endl;
+    string y_label = "../../../../mnist/regularized_label_small";
+    sketchy::armadillo y_train = sketchy::armadillo(y_label);
 
-    b.save("b"); 
-    x_tilde.save("x_tilde"); 
+    cout << "solving for b" << endl;
+    sketchy::armadillo b = x_train.solve_x(y_train);
+
+    cout << "solving for xtilde" << endl;
+    sketchy::armadillo x_tilde = sketchy::ops::lin_regress<sketchy::armadillo>(x_train, y_train, 600);
+
+    //sketchy::armadillo y_pred = x_train.mult(x_tilde);
+    //sketchy::armadillo y_pred2 = x_train.mult(b);
+
+    b.save("b");
+    x_tilde.save("x_tilde");
     */
-    //cout << (y_pred.subtract(y_pred2)).accumulate()  << endl; 
+    //cout << (y_pred.subtract(y_pred2)).accumulate()  << endl;
 
     return 0;
 }
